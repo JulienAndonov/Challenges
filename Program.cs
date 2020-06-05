8e8ff7e551b5dde50832594c36a64e0dd53fe548
JulienAndonov <julien.v.andonov@googlemail.com>
2020-06-05 12:15:25 +0200

    Update 05/06/2020

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PersonalTasks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inputValues = Console.ReadLine().Split(",",StringSplitOptions.RemoveEmptyEntries);
+            string inputString = inputValues[0];
+            int limit = int.Parse(inputValues[1]);
+
+            Console.WriteLine(stringReduction(inputString,limit));
+            
+        }
+
+        public static string stringReduction(string inputString, int limit)
+        {
+            var outputString = "";
+            int length = 0;
+
+
+            //aabbaa (1) => aba
+            //aaab (2) => aab
+
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if ((i > 0) && (inputString[i] == inputString[i - 1]))
+                {
+                    length++;
+                }
+                else
+                {
+                    length = 0;
+                }
+
+                if (length < limit)
+                {
+                    outputString += inputString[i];
+                }
+            }
+            return outputString;
+        }
+    }
+}
